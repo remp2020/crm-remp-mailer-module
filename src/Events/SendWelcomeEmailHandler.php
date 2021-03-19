50b3c2ca4aea73233595955db8f7a67ede4c724a
Zoltan Boraros <zoltan.boraros@shiny.house>
2021-03-19 14:34:30 +0100

    Add confirmation_url to registration-based notifications
    remp/crm#1743

--- src/Events/SendWelcomeEmailHandler.php
+++ src/Events/SendWelcomeEmailHandler.php
@@ -3,31 +3,49 @@
 namespace Crm\RempMailerModule\Events;
 
 use Crm\ApplicationModule\Hermes\HermesMessage;
+use Crm\UsersModule\Events\UserCreatedEvent;
+use Crm\UsersModule\Repository\UserEmailConfirmationsRepository;
 use League\Event\AbstractListener;
 use League\Event\EventInterface;
+use Nette\Application\LinkGenerator;
 use Tomaj\Hermes\Emitter;
 
 class SendWelcomeEmailHandler extends AbstractListener
 {
     private $hermesEmitter;
-
-    public function __construct(Emitter $hermesEmitter)
-    {
+    private $userEmailConfirmationsRepository;
+    private $linkGenerator;
+    
+    public function __construct(
+        Emitter $hermesEmitter,
+        UserEmailConfirmationsRepository $userEmailConfirmationsRepository,
+        LinkGenerator $linkGenerator
+    ) {
         $this->hermesEmitter = $hermesEmitter;
+        $this->userEmailConfirmationsRepository = $userEmailConfirmationsRepository;
+        $this->linkGenerator = $linkGenerator;
     }
 
     public function handle(EventInterface $event)
     {
-        $user = $event->getUser();
-
+        if (!$event instanceof UserCreatedEvent) {
+            throw new \Exception("Unable to handle event, expected UserCreatedEvent, received [" . get_class($event) . "]");
+        }
+        
         if ($event->sendEmail()) {
+            $user = $event->getUser();
+            $token = $this->userEmailConfirmationsRepository->getToken($user->id);
+            $link = $token ? $this->linkGenerator->link('Users:Users:EmailConfirm', ['token' => $token]) : null;
+            
             $this->hermesEmitter->emit(new HermesMessage('mailer-send-email', [
                 'email' => $user->email,
                 'mail_template_code' => 'welcome_email_with_password',
                 'params' => [
                     'email' => $user->email,
                     'password' => $event->getOriginalPassword(),
+                    'confirmation_url' => $link
                 ],
+                'attachments' => []
             ]));
         }
     }
