18f2d95487db606332a339d4bde834a4f3a9cabc
Peter Dulacka <dulacka@gmail.com>
2023-04-26 08:41:57 +0200

    Add user's mail logs to the personal data zip package
    remp/crm#2847

--- src/Models/User/RempMailerUserDataProvider.php
+++ src/Models/User/RempMailerUserDataProvider.php
@@ -4,18 +4,18 @@ namespace Crm\RempMailerModule\Models\User;
 
 use Crm\ApplicationModule\User\UserDataProviderInterface;
 use Crm\RempMailerModule\Models\Api\Client;
+use Crm\RempMailerModule\Models\Api\MailLogQueryBuilder;
+use Crm\RempMailerModule\Repositories\MailLogsRepository;
 use Crm\UsersModule\Repository\UsersRepository;
+use DateTimeInterface;
 
 class RempMailerUserDataProvider implements UserDataProviderInterface
 {
-    private $apiClient;
-
-    private $usersRepository;
-
-    public function __construct(Client $apiClient, UsersRepository $usersRepository)
-    {
-        $this->apiClient = $apiClient;
-        $this->usersRepository = $usersRepository;
+    public function __construct(
+        private Client $apiClient,
+        private UsersRepository $usersRepository,
+        private MailLogsRepository $mailLogsRepository,
+    ) {
     }
 
     public static function identifier(): string
@@ -30,7 +30,28 @@ class RempMailerUserDataProvider implements UserDataProviderInterface
 
     public function download($userId)
     {
-        return [];
+        $user = $this->usersRepository->find($userId);
+
+        $logQuery = new MailLogQueryBuilder();
+        $logQuery->setEmail($user->email);
+
+        $emailLogs = $this->mailLogsRepository->get($logQuery);
+
+        $result = [];
+        foreach ($emailLogs as $log) {
+            $result[] = array_filter([
+                'email' => $log->email,
+                'subject' => $log->subject,
+                'sent_at' => $log->sent_at->format(DateTimeInterface::RFC3339),
+                'delivered_at' => $log->delivered_at?->format(DateTimeInterface::RFC3339),
+                'opened_at' => $log->opened_at?->format(DateTimeInterface::RFC3339),
+                'clicked_at' => $log->clicked_at?->format(DateTimeInterface::RFC3339),
+                'spam_complained_at' => $log->spam_complained_at?->format(DateTimeInterface::RFC3339),
+                'hard_bounced_at' => $log->hard_bounced_at?->format(DateTimeInterface::RFC3339),
+            ]);
+        }
+
+        return $result;
     }
 
     public function downloadAttachments($userId)
