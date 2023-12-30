e3d9980ff59dcb84954d5e1145051a9010859628
Peter Dulacka <dulacka@gmail.com>
2023-12-30 13:07:57 +0100

    Fix log redaction after monolog update
    The context in Monolog's LogRecord is now readonly
    and any changes we attempted to make to it were
    not applied. To change the logged record, we need
    to make a new instance with changed properties.
    
    remp/crm#3062

--- src/Hermes/LogRedact.php
+++ src/Hermes/LogRedact.php
@@ -2,17 +2,20 @@
 
 namespace Crm\RempMailerModule\Hermes;
 
+use Monolog\LogRecord;
+
 class LogRedact
 {
     public static function add($filters)
     {
-        return function ($record) use ($filters) {
+        return function (LogRecord $record) use ($filters) {
+            $context = $record->context;
             foreach ($filters as $filter) {
-                if (isset($record['context']['payload']['params'][$filter])) {
-                    $record['context']['payload']['params'][$filter] = '******';
+                if (isset($context['payload']['params'][$filter])) {
+                    $context['payload']['params'][$filter] = '******';
                 }
             }
-            return $record;
+            return $record->with(context: $context);
         };
     }
 }
